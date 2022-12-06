80ff54400a6abe69c910007010f7a338249dc7f0
ClarisNjoga <mimonjoga@gmail.com>
2022-12-06 12:28:40 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const input = document.querySelector('input');
 
 function addingEventListener() {
-  input.addEventListener('click', function(event) {
-    return "clicked"
+  input.addEventListener('click', function() {
+    alert('I was clicked')
   });
 }
